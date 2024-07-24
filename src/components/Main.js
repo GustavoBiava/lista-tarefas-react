30d8f10c08a2f11784723e30898b4d8756305ffb
GustavoBiava <gustavo.rbiava@hotmail.com>
2024-07-24 19:42:04 -0300

    Task delete

--- src/components/Main.js
+++ src/components/Main.js
@@ -31,6 +31,16 @@ class Main extends Component {
     });
   };
 
+  handleDelete = (event, index) => {
+    const { tasks } = this.state;
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+
+    this.setState({
+      tasks: [...newTasks],
+    });
+  };
+
   render() {
     const { newTask, tasks } = this.state;
 
@@ -38,9 +48,10 @@ class Main extends Component {
       <div className="main">
         <h1>Lista de Tarefas</h1>
         <form action="" className="form" onSubmit={this.handleSubmit}>
-          <input onChange={this.handleInputChange}
-          type="text"
-          value={newTask}
+          <input
+            onChange={this.handleInputChange}
+            type="text"
+            value={newTask}
           />
           <button type="submit">
             <FaPlus />
@@ -48,12 +59,18 @@ class Main extends Component {
         </form>
 
         <ul className="tasks">
-          {tasks.map((task) => (
+          {tasks.map((task, index) => (
             <li key={task}>
               {task}
               <spam>
-                <FaEdit className="edit" />
-                <FaWindowClose className="delete" />
+                <FaEdit
+                  onClick={(event) => this.handleEdit(event, index)}
+                  className="edit"
+                />
+                <FaWindowClose
+                  onClick={(event) => this.handleDelete(event, index)}
+                  className="delete"
+                />
               </spam>
             </li>
           ))}
